20df76fa51349c3a8c8b42de0835d155f223b16b
Garrett <garrettdolson@gmail.com>
2014-10-24 11:08:07 -0700

    Add routes, controllers, and views for each nav item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
   root 'dashboard#index'
-  match 'classes', to: 'classes#index', via: [:get]
+  match 'dashboard',    to: 'dashboard#index',    via: [:get]
+  match 'appointments', to: 'appointments#index', via: [:get]
+  match 'classes',      to: 'classes#index',      via: [:get]
+  match 'workshops',    to: 'workshops#index',    via: [:get]
+  match 'clients',      to: 'clients#index',      via: [:get]
+  match 'products',     to: 'products#index',     via: [:get]
+  match 'services',     to: 'services#index',     via: [:get]
+  match 'staff',        to: 'staff#index',        via: [:get]
+  match 'business',     to: 'business#index',     via: [:get]
+  match 'admin',        to: 'admin#index',        via: [:get]
 
 end
