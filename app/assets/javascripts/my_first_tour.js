d073c2ff26f659828816a47856ec13bbb92ac488
Garrett Olson <garrettdolson@gmail.com>
2014-11-17 18:40:37 -0800

    Commit before adding project to Heroku

--- app/assets/javascripts/my_first_tour.js
+++ app/assets/javascripts/my_first_tour.js
@@ -5,7 +5,9 @@
       {
         target: "login-wrapper",
         title: "Welcome to Project Lotus!",
-        content: "This demo reimagines what the next version of our core software UI <em>might</em> look like. This (super quick) tour will show you around.",
+        content: "This demo reimagines what the next version of our\
+                core software UI <em>might</em> look like. This tour \
+                will show you around.",
         placement: "left",
         yOffset: "center",
         arrowOffset: 'center',
@@ -56,7 +58,7 @@
         title: "2. The sweet spot",
         content: "69% of a users focus (<em>i.e.</em> time) is spent\
                   on the left side of the screen. Putting the nav on\
-                  the left side puts it squarely in this area of focus area",
+                  the left side puts it squarely in this focal area.",
         placement: "right",
         yOffset: "center",
         arrowOffset: 'center',
@@ -65,8 +67,7 @@
         target: "nav-menu",
         title: "3. Intuitive workflows",
         content: "Left-side navigation leads to 'left to right' workflows,\
-                 a natural and familiar pattern to users in Western cultures. \
-                 This layout could also be reversed for 'right to left' cultures.",
+                 a natural and familiar pattern to Western cultures.",
         placement: "right",
         yOffset: "center",
         arrowOffset: 'center',
@@ -74,7 +75,8 @@
       {
         target: "business-tour-stop",
         title: "Tucked away",
-        content: "Multiple related services can be tucked away under a single menu link. Click the 'Appointments' link above to see its contents.",
+        content: "Multiple related services can be tucked away under a single menu link. \
+                <strong>Click</strong> the 'Appointments' link above to see its contents.",
         placement: "bottom",
         yOffset: -5,
         xOffset: 3,
@@ -86,7 +88,7 @@
         target: "business-tour-stop",
         title: "",
         delay: 200,
-        content: "Click 'Next' to navigate to the 'SLO Location' page shown above.",
+        content: "<strong>Click</strong> 'Next' to navigate to the 'SLO Location' page shown above.",
         placement: "bottom",
         yOffset: -10,
         xOffset: 3,
@@ -98,8 +100,8 @@
       },
       {
         target: "nav-toggle-button",
-        title: "Collapsibility",
-        content: "More screen size, you say? Click the arrow above to collapse the nav menu.",
+        title: "More screen size, you say?",
+        content: "For more minimal nav menu, click the arrow above to collapse the nav menu.",
         placement: "bottom",
         yOffset: -10,
         xOffset: -8,
@@ -118,9 +120,8 @@
       {
         target: "glance-menu",
         title: "Upcoming services widget",
-        content: "The 'upcoming services' widget dynamically updates information\
-                  on a set interval. Staff members can easily see who's coming in next\
-                  simply by looking at the top of the widget.",
+        content: "With the 'upcoming services' widget, staff members can easily\
+                  see who's coming in next simply by looking at the top of the list.",
         placement: "left", 
         yOffset: "center",
         arrowOffset: "center"
@@ -135,7 +136,7 @@
       {
         target: "checkout-btn",
         title: "Check, please",
-        content: "A 'shopping cart' icon in the upper left of a screen is instantly\
+        content: "The 'shopping cart' icon in the upper left of a screen is instantly\
                   recognizable to most users. Click on it to open up the checkout panel",
         placement: "left",
         yOffset: 3,
@@ -146,7 +147,6 @@
       },
       {
         target: "checkout-panel",
-        title: "Slide in, slide out",
         content: "The checkout panel slides into view from any page your on, cutting\
                   down on page loads.",
         placement: "left",
