6b6847aaa418db91d5dad5e5cb687ec4af844e3f
Garrett <garrettdolson@gmail.com>
2014-12-22 10:52:19 -0800

    Copy improvements for tours

--- app/assets/javascripts/my_first_tour.js
+++ app/assets/javascripts/my_first_tour.js
@@ -4,10 +4,9 @@
     steps: [
       {
         target: "login-wrapper",
-        title: "Welcome to Project Lotus!",
-        content: "This project imagines what a future version of our core software might look\
-                 like.<br><br>Once logged in, you can restart this tour by clicking\
-                 the staff member's name in the upper right, then 'Log out.'",
+        title: "Hey there!",
+        content: "Welcome to Project Lotus! This project imagines what a future version of our core software might look\
+                 like.",
         placement: "left",
         yOffset: "center",
         arrowOffset: 'center', 
@@ -16,9 +15,7 @@
       {
         target: "login-wrapper",
         title: "Sign In, simplified",
-        content: "The page you're looking at was inspired by the \
-                <a href='https://www.netflix.com/Login?locale=en-US&nextpage=http%3A%2F%2Fwww.netflix.com%2FYourAccount' target='_blank' class='external-link'>Netflix sign in page.</a>\
-                <br><br>A dedicated sign in page means everything not related to 'signing in' can be removed, resulting\
+        content: "A dedicated sign in page means everything not related to 'signing in' can be removed, resulting\
                 in a more zen-like experience.<br><br>\
                 Click 'Next' to sign in.",
         placement: "left",
@@ -32,11 +29,11 @@
       {
         target: "top-bar",
         title: "Responsive design. So hot right now.",
-        content: "Project Lotus is built on Bootstrap 3 and features response design: \
+        content: "Project Lotus is built on Bootstrap 3 and features response design, meaning \
                  the page layout changes to best fit the user's screen size. \
                  <br><br>Resize your browser window to check it out.",
         placement: "bottom",
-        width: 400,
+        width: 350,
         xOffset: "center",
         arrowOffset: 'center',
         showPrevButton: false,
@@ -45,34 +42,22 @@
       {
         target: "nav-menu",
         title: "Think vertically",
-        content: "A vertical nav menu offers some unique benefits to our users:<br><br>\
-                  <ul><li>Links take up consistent space and stack neatly on top of each other.</li>\
-                  <li>More links fit vertically than horizontally: goodbye, tabs-spilling-off-the-right-side-of-the-page.</li>\
-                  <li>Submenus group related services, simplifying user experience.</li>",
+        content: "A vertical nav menu offers some unique benefits:<br><br>\
+                  <ul> \
+                  <li>User experience is simplified by grouping related items in submenus.</li>\
+                  <li>More links show 'above the fold' compared to our current horizontal nav menu.</li>\
+                  <li>Placement on the far left puts navigation in a natural focal area where users spend \
+                  <a href='http://www.nngroup.com/articles/horizontal-attention-leans-left/'\
+                  target='_blank' class='external-link'>69% of their time.</a></li>\
+                  </ul>",
         placement: "right",
-        yOffset: "center",
-        arrowOffset: 'center',
-        width: 300
-      },
-      {
-        target: "nav-menu",
-        title: "Left side, best side",
-        content: "<blockquote>Web users spend <a href='http://www.nngroup.com/articles/horizontal-attention-leans-left/'\
-                  target='_blank' class='external-link'>69% of their time</a> viewing the left half of the page...\
-                  Keep navigation all the way to the left. This is where people look to \
-                  find a list of current options.</blockquote>\
-                  <a href='https://en.wikipedia.org/wiki/Jakob_Nielsen_(usability_consultant)' \
-                  target='_blank' class='external-link'>Jakob Neilsen, Ph.D.</a>\
-                  <br><em>User Advocate and principal of the Nielsen Norman Group,\
-                  a user experience consulting group</em>",
-        placement: "right",
-        width: 400,
+        width: 350,
         yOffset: "center",
         arrowOffset: 'center',
       },
       {
         target: "business-tour-stop",
-        title: "Menus",
+        title: "Submenus",
         content: "<strong>Click</strong> on the 'Appointments' link above to see what's nested under it.",
         placement: "bottom",
         yOffset: -5,
@@ -85,9 +70,9 @@
         target: "business-tour-stop",
         title: "Peek-a-boo",
         delay: 200,
-        content: "Animations, even something as simple as scrolling a menu into view, help people \
-                  understand how software works at a deeper level. <br><br>If they're implemented\
-                  in a unique and playful way, they can add wonder and curiosity to an interaction.",
+        content: "Animations help users understand how software works at a deeper level. \
+                 <br><br>If they're implemented in a unique and playful way, they can add \
+                 joy and wonder to a user's experience.",
         placement: "bottom",
         yOffset: -10,
         xOffset: 3,
@@ -110,7 +95,7 @@
       {
         target: "business-tour-stop",
         title: "Hover-ability",
-        content: "This is what it looks like when you hover over a link when the nav menu is collapsed.",
+        content: "This is what it looks like when a user hovers over a link with the nav menu collapsed.",
         placement: "top",
         yOffset: 3,
         xOffset: 3,
@@ -134,9 +119,15 @@
       {
         target: "glance-menu",
         title: "Upcoming Services",
-        content: "With the 'Upcoming Services' widget, staff members can easily\
-                  see what service is coming up next.<br><br> <strong>Click</strong>\
-                  the arrow to the right to hide Upcoming Services.",
+        content: "The 'Upcoming Services' widget allows staff to quickly and easily see \
+                  what client is coming in next.",
+        placement: "left", 
+        yOffset: "center",
+        arrowOffset: "center",
+      },
+      {
+        target: "glance-menu",
+        content: "<strong>Click</strong> the arrow to the right to collapse the 'Upcoming Services' widget.",
         placement: "left", 
         yOffset: "center",
         arrowOffset: "center",
@@ -155,9 +146,8 @@
       {
         target: "checkout-btn",
         title: "Check, please!",
-        content: "The 'shopping cart' icon and its placement in the upper right of a web page is instantly \
-                  recognizable to most users, signifying <em>the location where transactions\
-                  are completed.</em><br><br><strong>Click</strong> on the 'shopping cart' to open up the checkout panel.",
+        content: "The 'shopping cart' icon in the upper right is instantly recognizable to \
+                  most users.<br><br><strong>Click</strong> on it to open up the checkout panel.",
         placement: "left",
         yOffset: 3,
         xOffset: 15,
@@ -167,17 +157,20 @@
       },
       {
         target: "checkout-panel",
-        content: "The checkout panel slides in and out of view, reducing page loads.",
+        content: "The checkout panel slides in and out view, reducing page loads.<br><br>\
+                  In a completed state, this panel would display sections to lookup a client, \
+                  add items to the cart, choose a payment method, and complete a transaction.",
         placement: "left",
         yOffset: "center",
         arrowOffset: "center",
+        nextOnTargetClick: false,
+        showNextButton: true,
         delay: 200
       },
       {
         target: "top-bar",
         title: "All done!",
-        content: "Feel free to cruise around and click on things! If you'd like to restart\
-                  the tour, click the staff member's name (Alyx Vance) in the upper right, \
+        content: "To restart the tour, click the staff member's name (Alyx Vance) in the upper right, \
                   then click 'Log Out'.",
         placement: "bottom",
         width: 350,
